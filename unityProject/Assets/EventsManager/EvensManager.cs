c7f5d827279fd4b9f0bffea00455b1dcaaf2d4d3
Gianluca <gianluca.destefano@studenti.unitn.it>
2020-01-31 23:04:28 +0100

    refined event

---
+++ unityProject/Assets/EventsManager/EvensManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+    private static EventManager _instance;
+    public float tick = 1; //time interval in seconds between the update 
+    List<Event> activeEvents = new List<Event>(); //list containing all the events currently in execution
+
+    public static EventManager Instance
+    {
+        get { return _instance; }
+    }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+    public void Start()
+    {
+        InvokeRepeating("ExecuteEvents", 1, 1);
+    }
+
+    //add a new event to the event that are executing
+    public void FireEvent(Event newEvent)
+    {
+        newEvent.onEventStarts();
+        this.activeEvents.Add(newEvent);
+    }
+
+    //Execute all the active ecents and deactivate them if they have done their time
+    public void ExecuteEvents()
+    {
+        foreach (var ev in activeEvents)
+        {    
+            ev.onEventExecute();
+            if (ev.startTime + ev.duration >= Time.time)
+            {
+                ev.onEventEnd();
+                activeEvents.Remove(ev);
+            }
+        }    
+    }
+    
+    
+}
---
+++ unityProject/Assets/EventsManager/Event.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Event", menuName = "Event", order = 1)]
+public class Event : ScriptableObject
+{
+	[Tooltip("Name of the event")]
+	public string name;
+	
+	[Multiline]
+	[Tooltip("Description of the event")]
+	public string description;
+
+	[Tooltip("Duration of the event in seconds")]
+	public int duration = 20;
+	
+	[Tooltip("Percentage debuf to population")]
+	[Range(0, 1)] public float deltaPoppulation = 0;
+
+	[Tooltip("Percentage debut to nature")]
+	[Range(0, 1)] public float deltaNature = 0;
+	
+	[Tooltip("Percentage debuf to temperature")]
+	[Range(0, 1)] public float deltaTemperature= 0;
+	
+	[Tooltip("Percentage debuf to Water")]
+	[Range(0, 1)] public float deltaWater = 0;
+	
+	[Tooltip("Percentage debuf to percentage")]
+	[Range(0, 1)] public float deltaResources = 0;
+
+	private float startTimeValue = 0; //time the event fired
+
+	private int currentPopulationVariation = 0; //how many people have died for this event
+	private int targetPopulationVariation = 0; //how many people will be dead because of this event at the end of its execution
+
+	private int currentNatureVariation = 0; // nature score variation caused by this event up to now
+	private int targetNatureVariation = 0;// nature score variation at the end of this event
+	
+	private int currentTemperatureVariation = 0; //temperature score variation since the start of the event
+	private int targetTemperatureVariation = 0; //target temperature score variation at the end of the event
+	
+	private int currentWaterVariation = 0; //water score variation since the start of the event
+	private int targetWaterVariation = 0; //target water score variation at the end of the event
+	
+	private int currentResourcesVariation = 0; //resources score variation since the start of the event
+	private int targetResourceVariation = 0; //target resource score variation at the end of the event
+	
+	public float startTime 
+	{
+		get{ return this.startTimeValue; }
+	}
+	
+	public void onEventStarts()
+	{
+		this.startTimeValue = Time.time;
+		/*
+		this.targetPopulationVariation = ;
+		this.targetNatureVariation = ;
+		this.targetTemperatureVariation = ;
+		this.targetWaterVariation =;
+		this.targetResourceVariation = ;
+		*/
+		
+	}
+	
+	public void onEventExecute()
+	{
+		
+	}
+
+	public void onEventEnd()
+	{
+		throw new NotImplementedException();
+	}
+	
+}
