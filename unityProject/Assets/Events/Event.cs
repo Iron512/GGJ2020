c3a8ea212bdfc2d22a0dee8497a4e537b99893ca
Gianluca <gianluca.destefano@studenti.unitn.it>
2020-01-31 21:34:45 +0100

    initial event class implementation

---
+++ unityProject/Assets/Events/Event.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Event", menuName = "Event", order = 1)]
+public class Event : ScriptableObject
+{
+	[Tooltip("Name of the event")]
+	public string name;
+	
+	[Multiline]
+	[Tooltip("Description of the event")]
+	public string description;
+
+	public int duration = 20;
+	[Range(0, 1)] public float deltaPoppulation;
+	[Range(0, 1)] public float deltaIndustry;
+	[Range(0, 1)] public float deltaClimate;
+}
