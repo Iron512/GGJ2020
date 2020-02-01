bd88f3a37302fc799c00ce909682c503a0cd52e6
GabrielePasolli <gabriele.pasolli@gmail.com>
2020-02-01 20:39:19 +0100

    added main menu

---
+++ unityProject/Assets/MainMenuScripts/SceneSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void GoToCreditsScene()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
