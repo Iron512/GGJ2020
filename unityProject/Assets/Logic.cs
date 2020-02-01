dd5758d86629aa79eec2db716eceb0e61409c0db
iron512 <ivanmartini96@gmail.com>
2020-02-01 23:13:57 +0100

    Logica aggiunta ma è spaccata

---
+++ unityProject/Assets/Logic.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Logic : MonoBehaviour {
+	public Scrollbar healthScroll;
+	public Scrollbar industryScroll;
+	public Scrollbar anarchyScroll;
+	public Scrollbar greenScroll;
+	public Scrollbar researchScroll;
+
+	private float welfare = 50f;
+	private float popularity = 50f;
+	private float nature = 50f;
+	private float water = 50f;
+	private float production = 50f;
+
+	private float[,] policies = {
+		{0.2f,0.1f,-0.05f,-0.05f,-0.1f,0}, //salute
+		{-0.1f,0.1f,-0.05f,-0.05f,0.2f,0}, //industriella
+		{-0.05f,-0.05f,0.1f,0.1f,0,0}, //analchia
+		{0.1f,0,0.1f,0.1f,-0.2f,0}, //green
+		{-0.1f,-0.1f,-0.1f,-0.1f,-0.1f,0.5f} //ricerca
+	};
+
+    // Start is called before the first frame update
+    void Start() {
+        InvokeRepeating("executeTick",3,1);
+    }
+
+    void executeTick() {
+    	float health = healthScroll.value;
+    	float industry = industryScroll.value;
+    	float anarchy = anarchyScroll.value;
+    	float green = greenScroll.value;
+    	float research = researchScroll.value;
+
+    	float deltaWelfare = 0;
+    	float deltaPopularity = 0;
+    	float deltaNature = 0;
+    	float deltaWater = 0;
+    	float deltaProduction = 0;
+
+    	for (int i = 0; i < 5; i++) {
+    		deltaWelfare += policies[i,0] * health;
+    		deltaPopularity += policies[i,1] * industry;
+    		deltaNature += policies[i,2] * anarchy;
+    		deltaWater += policies[i,3] * green;
+    		deltaProduction += policies[i,4] * research;
+    	}
+
+    	welfare *= (1 + deltaWelfare);
+    	popularity *= (1 + deltaPopularity);
+    	nature *= (1 + deltaNature);
+    	water *= (1 + deltaWater);
+    	production *= (1 + deltaProduction);
+
+
+    	Circle._instance.changeWelfare(welfare);
+    	Circle._instance.changePopularity(popularity);
+    	Circle._instance.changeNature(nature);
+    	Circle._instance.changeWater(water);
+    	Circle._instance.changeProduction(production);
+    }
+}
--- unityProject/Assets/ResourcesManager/ResourcesManager.cs
+++ unityProject/Assets/ResourcesManager/ResourcesManager.cs
@@ -4,27 +4,117 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ResourcesManager : Singleton<ResourcesManager> {
+public class ResourcesManager : Singleton<ResourcesManager>
+{
+    // access to singleton:
+    // ResourceManager.Instance.ResourceTestInt
+    // Basic
 
-    private int welfare = 50;
-    private int nature = 50;
-    private int water = 50;
-    private int production = 50;
-    private int popularity = 50;
+    //public float coeficient = 1f;
 
-    private int[,] policies = new int {
-        {20;10;-5;-5;-10},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {0,0,0,0,0},
-        {-10;-10;-10;-10;-10}
-    };
+    public int population = 1000; // population count, impacts directly wellfare, natrue, water, resources
+    
+    //players parameters
+    [Range(0, 1)] public float wellfare = .25f; // impacts one the population growth
+    [Range(0, 1)] public float nature = .25f; // lower the pollution
+    [Range(0, 1)] public float water = .25f; // impacts on the population and the nature
+    [Range(0, 1)] public float resources = .25f; // impacts on the population
+    //[Range(0, 1)] public float education = .2f; // impacts on the thresholds of nature water and resources
+    [Range(0, 1)] public float reasearch = .2f; // ammount of the economy dedicated to the research, allows to unlock new technologies
 
-    public void Start() {
-        InvokeRepeating("executeTick",2,1);
+
+    [Tooltip("How important is the walfare for the population growth?")]
+    [Range(0, 1)] public float wellfareImportance = .4f;
+    [Tooltip("How important is the nature for the population growth?")]
+    [Range(0, 1)] public float natureImportance = .2f;
+    [Tooltip("How important is the water for the population growth?")]
+    [Range(0, 1)] public float waterImportance = .2f;
+    [Tooltip("How important is the resource for the population growth?")]
+    [Range(0, 1)] public float resourcesImportance = .2f;
+    
+    //pollution level
+    [Range(0, 1)]
+    public float pollution = 0f; //hit negatively nature, water, resources, wellfare 
+    
+    public AnimationCurve resourceImpact;
+
+    public AnimationCurve pollutionImpact;
+
+    public AnimationCurve natureBenefit;
+    // Moltiplicators
+    public float wellfareMoltiplicator = 1f;
+    public float natureMoltiplicator = 1f;
+    public float pollutionMoltiplicator = 1f;
+    public float waterMoltiplicator = 1f;
+    public float resourceMoltiplicator = 1f;
+
+    public void Start()
+    {
+        InvokeRepeating("executeTick",2,3);
     }
 
-    public void executeTick() {
+    public void executeTick()
+    {
+        //update the population 
+        //this.population = (int)(this.population + (this.wellfare - this.getWellfareThreshold()*this.population/10));
+        float pollutionFactor = pollutionImpact.Evaluate(pollution);
+        
+        float wellfareFactor =  wellfareImportance*applyPollution(resourceImpact.Evaluate(this.wellfare / this.idealWellfate),pollutionFactor);
+        float waterFactor =  waterImportance*applyPollution(resourceImpact.Evaluate(this.water / this.idealWater),pollutionFactor);
+        float resourceFactor =  resourcesImportance*applyPollution(resourceImpact.Evaluate(this.resources / this.idealResources),pollutionFactor);
+        
+        float populationFactor =wellfareFactor + waterFactor + resourceFactor;
+        
+        float natureFactor = natureImportance*resourceImpact.Evaluate(this.nature / this.idealNature);
 
+        if (this.pollution > 0)
+        {
+            this.pollution -= Mathf.Max(0.01f, this.natureBenefit.Evaluate(this.nature)*this.nature);
+            if (this.pollution < 0)
+            {
+                this.pollution = 0;
+            }
+        }
+        
+        this.population = (int)(this.population + this.population / 30 * populationFactor);
+        string s = this.population + "," + populationFactor + "," + wellfareFactor + "," + waterFactor + "," +
+                   natureFactor + "," + resourceFactor;
+        
+            }
+
+    private float applyPollution(float factor,float pollution)
+    {
+        if (factor < 0)
+        {
+            return factor * (pollution + 1);
+        }
+        else
+        {
+            return factor * (1-pollution);
+        }
+    }
+    public float idealNature
+    {
+        get { return (0.25f); }
+    }
+
+    public float idealWellfate
+    {
+        get{ return  (0.25f); }
+    }
+    
+    public float idealPollution
+    {
+        get{return 0; }
+    }
+
+    public float idealWater
+    {
+        get { return (.25f); }
+    }
+
+    public float idealResources
+    {
+        get { return (.25f); }
     }
 }
