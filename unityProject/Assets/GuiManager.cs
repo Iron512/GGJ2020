9ca05e555fc4d8f3a0c8ea98ad183fc8e635e2c9
Gianluca <gianluca.destefano@studenti.unitn.it>
2020-02-02 11:07:01 +0100

    initial gui manager

---
+++ unityProject/Assets/GuiManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiManager : Singleton<GuiManager>
+{
+    // Start is called before the first frame update
+    public GameObject inGameGui;
+    public GameObject researchTreeGui;
+
+
+    public void showReasearchTree(){
+        researchTreeGui.gameObject.SetActive(true);
+        inGameGui.gameObject.SetActive(false);
+    }
+    
+    public void showInGameGui(){
+        researchTreeGui.gameObject.SetActive(false);
+        inGameGui.gameObject.SetActive(true);
+    }
+}
