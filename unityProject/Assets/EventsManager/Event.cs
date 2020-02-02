c845ca50535e7eb39e1fcfd3935c0679662a5c9a
GabrielePasolli <gabriele.pasolli@gmail.com>
2020-02-02 14:55:25 +0100

    WIP events

--- unityProject/Assets/EventsManager/Event.cs
+++ unityProject/Assets/EventsManager/Event.cs
@@ -20,34 +20,34 @@ public class Event : ScriptableObject
 	[Tooltip("Percentage debuf to population")]
 	[Range(0, 1)] public float deltaWelfare = 0;
 
-	[Tooltip("Percentage debut to nature")]
+	[Tooltip("Percentage debut to popularity")]
 	[Range(0, 1)] public float deltaPopularity = 0;
 	
-	[Tooltip("Percentage debuf to temperature")]
+	[Tooltip("Percentage debuf to nature")]
 	[Range(0, 1)] public float deltaNature= 0;
 	
 	[Tooltip("Percentage debuf to Water")]
 	[Range(0, 1)] public float deltaWater = 0;
 	
-	[Tooltip("Percentage debuf to percentage")]
+	[Tooltip("Percentage debuf to production")]
 	[Range(0, 1)] public float deltaProduction = 0;
 
 	protected float startTime = 0; //time the event fired
 
-	private int currentPopulationVariation = 0; //how many people have died for this event
-	private int targetPopulationVariation = 0; //how many people will be dead because of this event at the end of its execution
+	private int currentWelfareVariation = 0; //how many people have died for this event
+	private int targetWelfareVariation = 0; //how many people will be dead because of this event at the end of its execution
+	
+	private int currentPopularityVariation = 0; //temperature score variation since the start of the event
+	private int targetPopularityVariation = 0; //target temperature score variation at the end of the event
 
 	private int currentNatureVariation = 0; // nature score variation caused by this event up to now
 	private int targetNatureVariation = 0;// nature score variation at the end of this event
 	
-	private int currentPollutionVariation = 0; //temperature score variation since the start of the event
-	private int targetPollutionVariation = 0; //target temperature score variation at the end of the event
-	
 	private int currentWaterVariation = 0; //water score variation since the start of the event
 	private int targetWaterVariation = 0; //target water score variation at the end of the event
 	
-	private int currentResourcesVariation = 0; //resources score variation since the start of the event
-	private int targetResourceVariation = 0; //target resource score variation at the end of the event
+	private int currentProductionVariation = 0; //resources score variation since the start of the event
+	private int targetProducitonVariation = 0; //target resource score variation at the end of the event
 
 	public float timeAtStart
 	{
@@ -65,12 +65,11 @@ public class Event : ScriptableObject
 		
 
 		//set all target variations
-		/*this.targetPopulationVariation = (int)(ResourcesManager._instance.population * (1 - this.deltaPoppulation));
-		this.targetNatureVariation = (int)(ResourcesManager._instance.nature * (1 - this.deltaPoppulation));
-		this.targetPollutionVariation = (int)(ResourcesManager._instance.pollution * (1 - this.deltaPoppulation));
-		this.targetWaterVariation = (int)(ResourcesManager._instance.water * (1 - this.deltaPoppulation));
-		this.targetResourceVariation = (int)(ResourcesManager._instance.resources * (1 - this.deltaPoppulation));
-		*/
+		this.targetWelfareVariation = (int)(ResourcesManager._instance.welfare * (1 - this.deltaWelfare));
+		this.targetPopularityVariation = (int)(ResourcesManager._instance.popularity * (1 - this.deltaPopularity));
+		this.targetNatureVariation = (int)(ResourcesManager._instance.nature * (1 - this.deltaNature));
+		this.targetWaterVariation = (int)(ResourcesManager._instance.water * (1 - this.deltaWater));
+		this.deltaProduction = (int)(ResourcesManager._instance.production * (1 - this.deltaProduction));
 	}
 	
 	
@@ -79,22 +78,20 @@ public class Event : ScriptableObject
 	{
 		float percentage = Mathf.Min(Time.time-this.startTime/this.duration,1);
 
-		/*ResourcesManager._instance.population -= this.computeResourceValue(ref this.targetPopulationVariation,
-			ref this.currentPopulationVariation, percentage);
+		ResourcesManager._instance.welfare -= this.computeResourceValue(ref this.targetWelfareVariation,
+			ref this.currentWelfareVariation, percentage);
+
+		ResourcesManager._instance.popularity -= this.computeResourceValue(ref this.targetPopularityVariation,
+			ref this.currentPopularityVariation, percentage);
 
 		ResourcesManager._instance.nature -= this.computeResourceValue(ref this.targetNatureVariation,
 			ref this.currentNatureVariation, percentage);
 		
-		ResourcesManager._instance.pollution -= this.computeResourceValue(ref this.targetPollutionVariation,
-			ref this.currentPollutionVariation, percentage);
-		
 		ResourcesManager._instance.water -= this.computeResourceValue(ref this.targetWaterVariation,
 			ref this.currentWaterVariation, percentage);
 		
-		ResourcesManager._instance.resources -= this.computeResourceValue(ref this.targetResourceVariation,
-			ref this.currentResourcesVariation, percentage);
-			
-			*/
+		ResourcesManager._instance.production -= this.computeResourceValue(ref this.targetProducitonVariation,
+			ref this.currentProductionVariation, percentage);
 	}
 
 	private int computeResourceValue(ref int targetVariation, ref int currentVariation, float percentage)
--- unityProject/Assets/ResourcesManager/ResourcesManager.cs
+++ unityProject/Assets/ResourcesManager/ResourcesManager.cs
@@ -14,11 +14,11 @@ public class ResourcesManager : Singleton<ResourcesManager>
 	public Scrollbar greenScroll;
 	public Scrollbar researchScroll;
 
-	private float welfare = 50f;
-	private float popularity = 50f;
-	private float nature = 50f;
-	private float water = 50f;
-	private float production = 50f;
+	public float welfare = 50f;
+	public float popularity = 50f;
+	public float nature = 50f;
+	public float water = 50f;
+	public float production = 50f;
 
 	private float[,] policies = {
 		{0.2f,0.1f,-0.05f,-0.05f,-0.1f,0}, //salute
