586d4f004811415989c1fb7a30b9971af4c81304
GabrielePasolli <gabriele.pasolli@gmail.com>
2020-02-02 12:21:40 +0100

    added event cards form managers

---
+++ unityProject/Assets/EventFormManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventFormManager : MonoBehaviour
+{
+    public Text descriptionLabel;
+    public Text nameLabel;
+    public Image icon;
+    public Event currentEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void SetNewEvent(Event newEvent)
+    {
+        this.descriptionLabel.text = newEvent.description;
+        this.nameLabel.text = newEvent.name;
+        this.icon.sprite = newEvent.icon;
+        this.currentEvent = newEvent;
+    }
+}
