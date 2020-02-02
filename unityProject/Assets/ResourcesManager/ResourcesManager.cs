4d9452c6deb03fb9079e6e41a4ab3036ff175e59
Francesco Zardi <frazar00@gmail.com>
2020-02-02 16:17:37 +0100

    Add background music

--- unityProject/Assets/ResourcesManager/ResourcesManager.cs
+++ unityProject/Assets/ResourcesManager/ResourcesManager.cs
@@ -34,7 +34,7 @@ public class ResourcesManager : Singleton<ResourcesManager>
 
     // Start is called before the first frame update
     void Start() {
-        InvokeRepeating("executeTick",3,1);
+        InvokeRepeating("executeTick", 3, 0.1f);
     }
 
     void executeTick() {
