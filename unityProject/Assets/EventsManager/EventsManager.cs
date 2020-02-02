1680505ac156952e86a8d659a921a08548193f58
GabrielePasolli <gabriele.pasolli@gmail.com>
2020-02-02 10:03:50 +0100

    added event activator

--- unityProject/Assets/EventsManager/EventsManager.cs
+++ unityProject/Assets/EventsManager/EventsManager.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventsManager : MonoBehaviour {
+public class EventsManager : MonoBehaviour
+{
     private static EventsManager _instance;
     public float tick = 1; //time interval in seconds between the update 
+    public float secondsToEventStart = 5; // time interval in seconds between each event
     List<Event> activeEvents = new List<Event>(); //list containing all the events currently in execution
+    public List<Event> availableEvents;
 
     public static EventsManager Instance
     {
@@ -19,17 +22,27 @@ public class EventsManager : MonoBehaviour {
         if (_instance != null && _instance != this)
         {
             Destroy(this.gameObject);
-        } else {
+        }
+        else
+        {
             _instance = this;
         }
     }
 
-    
+
     public void Start()
     {
+        print("start eventmanager");
         InvokeRepeating("ExecuteEvents", 1, tick);
+        InvokeRepeating("FireRandomEvent", 1, secondsToEventStart);
     }
 
+    // starts a random event
+    public void FireRandomEvent()
+    {
+        Event eventToStart = GetEvent();
+        FireEvent(eventToStart);
+    }
     //add a new event to the event that are executing
     public void FireEvent(Event newEvent)
     {
@@ -41,14 +54,20 @@ public class EventsManager : MonoBehaviour {
     public void ExecuteEvents()
     {
         foreach (var ev in activeEvents)
-        {    
+        {
             ev.onEventExecute();
             if (ev.timeAtStart + ev.duration >= Time.time)
             {
                 ev.onEventEnd();
                 activeEvents.Remove(ev);
             }
-        }    
+        }
     }
-
+    public Event GetEvent()
+    {
+        // private Utility utility = new Utility;
+        var random = new System.Random();
+        int randomIndex = random.Next(availableEvents.Count);
+        return availableEvents[randomIndex];
+    }
 }
